72b54aa653ab61a2b2ba3d3eabc549ed9421ad23
pogin503 <pogin503@gmail.com>
2013-11-19 17:44:41 +0900

    bot.rbのコピペで動くかどうか
    ref: https://github.com/raa0121/ruby-lingrbot/blob/master/bot.rb

--- web.rb
+++ web.rb
@@ -8,24 +8,58 @@ get '/' do
   "Hello, world"
 end
 
-def reply(data={})
-  msg = ""
-  if data["status"] == "ok" and data["events"]
-    data["events"].each do |e, text=e["message"]["text"]|
-      text.match(/(ぬるぽ|ヌルポ|null)/i) do
-        msg = "ガ"
-        break
+# def reply(data={})
+#   msg = ""
+#   if data["status"] == "ok" and data["events"]
+#     data["events"].each do |e, text=e["message"]["text"]|
+#       text.match(/(ぬるぽ|ヌルポ|null)/i) do
+#         msg = "ガ"
+#         break
+#       end
+#     end
+#   end
+#   msg
+# rescue
+#   ""
+# end
+
+def sandbox(&code)
+  proc {
+    $SAFE = 2
+    eval &code
+  }.call
+
+end
+
+get '/' do
+  "Here is raa0121's sinatra app"
+end
+
+post '/' do
+  content_type :text
+  json = JSON.parse(request.body.read)
+  json["events"].map do |e|
+    if e["message"]
+      m = e["message"]["text"]
+      if /^!\s?(.*)/ =~ m
+        x = Thread.start do
+          $SAFE = 2
+          eval "#{$1}"
+        end
+          "#{x.value}"
+          #sandbox{$1}
       end
     end
   end
-  msg
-rescue
-  ""
 end
 
-post "/do" do
-    request.body.rewind  # in case someone already read it
-    data = (JSON.parse request.body.read rescue {})
 
-    reply(data)
-end
+# post "/do" do
+#     request.body.rewind  # in case someone already read it
+#     data = (JSON.parse request.body.read rescue {})
+
+#     reply(data)
+# end
+# def lingr_
+
+# end
