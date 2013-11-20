526a254f23a09c882288e8df788af549ada3b2f7
pogin503 <pogin503@gmail.com>
2013-11-20 12:52:56 +0900

    erro code 500 を返さないようになるかの実験

--- web.rb
+++ web.rb
@@ -4,32 +4,29 @@ require "json"
 require "open-uri"
 require 'open3'
 
-def sandbox(&code)
-  proc {
-    $SAFE = 2
-    eval &code
-  }.call
+def reply(data={})
+    msg = ""
+    if data["status"] == "ok" and data["events"]
+        data["events"].each do |e, text=e["message"]["text"]|
+            text.match(/(ぬるぽ|ヌルポ|null)/i) do
+                msg = "ガ"
+                break
+            end
+        end
+    end
+    msg
+rescue
+    ""
+end
 
+post "/" do
+  # request.body.rewind  # in case someone already read it
+  json = JSON.parse(request.body.string)
+  # data = (JSON.parse request.body.read rescue {})
+  json
+  # reply(data)
 end
 
 get '/' do
   "Here is pogin's sinatra app"
-end
-
-post '/' do
-  content_type :text
-  json = JSON.parse(request.body.read)
-  json["events"].map do |e|
-    if e["message"]
-      m = e["message"]["text"]
-      if /^!\s?(.*)/ =~ m
-        x = Thread.start do
-          $SAFE = 2
-          eval "#{$1}"
-        end
-          "#{x.value}"
-          #sandbox{$1}
-      end
-    end
-  end
 end
